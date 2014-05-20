4126534c9915438e203144a988129a1ef6e3d064
renzon <renzon@gmail.com>
2014-05-20 12:26:15 -0300

    #6 rename groups to permissions, so it can be used for this issue

--- project_template/backend/src/web/home.py
+++ project_template/backend/src/web/home.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
 from tekton import router
-from web import groups
+from web import permission
 from web.login import home
 
 
@@ -11,9 +11,9 @@ from web.login import home
 @no_csrf
 def index(_write_tmpl):
     _write_tmpl('home.html', {'login_path': router.to_path(home.index),
-                              'no_login_path': router.to_path(groups.nologin),
-                              'no_permission_path': router.to_path(groups.nopermission),
-                              'adm_path': router.to_path(groups.admin),
-                              'adm_or_manager_path': router.to_path(groups.admin_or_manager),
-                              'manager_path': router.to_path(groups.manager),
-                              'sys_owner_path': router.to_path(groups.sysowner)})
+                              'no_login_path': router.to_path(permission.nologin),
+                              'no_permission_path': router.to_path(permission.nopermission),
+                              'adm_path': router.to_path(permission.admin),
+                              'adm_or_manager_path': router.to_path(permission.admin_or_manager),
+                              'manager_path': router.to_path(permission.manager),
+                              'sys_owner_path': router.to_path(permission.sysowner)})
