8365948126c065c50159288caacc40b4d5eb46ef
renzon <renzon@gmail.com>
2014-06-05 21:55:27 -0300

    #12 Screen informing email problens ok

--- project_template/backend/src/web/login/google.py
+++ project_template/backend/src/web/login/google.py
@@ -8,8 +8,11 @@ from gaepermission.decorator import login_not_required
 
 @login_not_required
 @no_csrf
-def index(_handler, _resp):
+def index(_handler, _resp, _write_tmpl):
     user = users.get_current_user()
     if user:
-        facade.login_google(user, _resp).execute()
+        cmd = facade.login_google(user, _resp).execute()
+        if cmd.pending_link:
+            _write_tmpl('login/pending.html', {'provider': 'Google', 'email': user.email()})
+            return
     _handler.redirect('/')
---
+++ project_template/backend/src/web/login/pending.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from gaecookie.decorator import no_csrf
+from gaepermission.decorator import login_not_required
+
+
+@no_csrf
+@login_not_required
+def index(pending_link_id):
+    pass
