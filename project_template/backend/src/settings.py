fec862ec6c84e19bf6b529f1509142c4b39236bf
renzon <renzon@gmail.com>
2014-07-01 01:23:08 -0300

    #19 settings, template and convention ok

--- project_template/backend/src/settings.py
+++ project_template/backend/src/settings.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from config.tmpl_middleware import TemplateWriteMiddleware, TemplateMiddleware
 from gaecookie.middleware import CSRFMiddleware, CSRFInputToDependency
 from gaepermission.middleware import LoggedUserMiddleware, PermissionMiddleware
-from tmpl_middleware import TemplateMiddleware
 from tekton.gae.middleware.email_errors import EmailMiddleware
-from tekton.gae.middleware.json_middleware import JsonMiddleare
+from tekton.gae.middleware.json_middleware import JsonResponseMiddleware
 from tekton.gae.middleware.parameter import RequestParamsMiddleware
+from tekton.gae.middleware.redirect import RedirectMiddleware
 from tekton.gae.middleware.router_middleware import RouterMiddleware, ExecutionMiddleware
 from tekton.gae.middleware.webapp2_dependencies import Webapp2Dependencies
 
 SENDER_EMAIL = 'renzon@gmail.com'
+TEMPLATE_404_ERROR = 'base/404.html'
+TEMPLATE_400_ERROR = 'base/400.html'
 
 MIDDLEWARES = [LoggedUserMiddleware,
                TemplateMiddleware,
-               JsonMiddleare,
                EmailMiddleware,
                Webapp2Dependencies,
                RequestParamsMiddleware,
@@ -21,5 +23,9 @@ MIDDLEWARES = [LoggedUserMiddleware,
                RouterMiddleware,
                CSRFMiddleware,
                PermissionMiddleware,
-               ExecutionMiddleware]
+               ExecutionMiddleware,
+               TemplateWriteMiddleware,
+               JsonResponseMiddleware,
+               RedirectMiddleware]
+
 
